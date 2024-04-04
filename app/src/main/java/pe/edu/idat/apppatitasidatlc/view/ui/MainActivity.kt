17850c2289eb75b49d84bee13f4ed92ea8d7aa28
cuartociclo2023 <cuartocicloidat2023@gmail.com>
2024-04-03 22:19:38 -0500

    login con API Rest

--- app/src/main/java/pe/edu/idat/apppatitasidatlc/view/ui/MainActivity.kt
+++ app/src/main/java/pe/edu/idat/apppatitasidatlc/view/ui/MainActivity.kt
@@ -1,12 +1,55 @@
 package pe.edu.idat.apppatitasidatlc.view.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import pe.edu.idat.apppatitasidatlc.R
+import pe.edu.idat.apppatitasidatlc.databinding.ActivityMainBinding
+import pe.edu.idat.apppatitasidatlc.retrofit.response.LoginResponse
+import pe.edu.idat.apppatitasidatlc.util.AppMensaje
+import pe.edu.idat.apppatitasidatlc.util.TipoMensaje
+import pe.edu.idat.apppatitasidatlc.viewmodel.AuthViewModel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var authViewModel: AuthViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        authViewModel = ViewModelProvider(this)
+            .get(AuthViewModel::class.java)
+        binding.btnlogin.setOnClickListener(this)
+        binding.btnregistro.setOnClickListener(this)
+        authViewModel.loginResponse.observe(this, Observer {
+            response -> obtenerDatosLogin(response)
+        })
+    }
+
+    private fun obtenerDatosLogin(response: LoginResponse) {
+        if(response.rpta){
+            startActivity(Intent(applicationContext, HomeActivity::class.java))
+        }else{
+            AppMensaje.enviarMensaje(binding.root, response.mensaje, TipoMensaje.ERROR)
+        }
+        binding.btnlogin.isEnabled = true
+        binding.btnregistro.isEnabled = true
+    }
+
+    override fun onClick(v: View) {
+        when(v.id){
+            R.id.btnlogin -> autenticarUsuario()
+            R.id.btnregistro -> startActivity(Intent(applicationContext, RegistroActivity::class.java))
+        }
+
+    }
+    private fun autenticarUsuario() {
+        binding.btnlogin.isEnabled = false
+        binding.btnregistro.isEnabled = false
+        authViewModel.login(binding.etusuario.text.toString(),
+            binding.etpassword.text.toString())
     }
 }
--- app/src/main/java/pe/edu/idat/apppatitasidatlc/viewmodel/AuthViewModel.kt
+++ app/src/main/java/pe/edu/idat/apppatitasidatlc/viewmodel/AuthViewModel.kt
@@ -1,4 +1,28 @@
 package pe.edu.idat.apppatitasidatlc.viewmodel
 
-class AuthViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import pe.edu.idat.apppatitasidatlc.repository.AuthRepository
+import pe.edu.idat.apppatitasidatlc.retrofit.request.LoginRequest
+import pe.edu.idat.apppatitasidatlc.retrofit.request.RegistroRequest
+import pe.edu.idat.apppatitasidatlc.retrofit.response.LoginResponse
+import pe.edu.idat.apppatitasidatlc.retrofit.response.RegistroResponse
+
+class AuthViewModel : ViewModel() {
+    var loginResponse: LiveData<LoginResponse>
+    var registroResponse: LiveData<RegistroResponse>
+    private var repository = AuthRepository()
+    init {
+        loginResponse = repository.loginResponse
+        registroResponse = repository.registroResponse
+    }
+    fun login(usuario:String, password:String){
+        loginResponse = repository.login(LoginRequest(usuario, password))
+    }
+    fun registro(nombres:String, apellidos:String,
+                 email:String, celular:String,
+                 usuario:String,password: String){
+        registroResponse = repository.registro(RegistroRequest(nombres,apellidos,email,
+            celular, password, usuario))
+    }
 }
