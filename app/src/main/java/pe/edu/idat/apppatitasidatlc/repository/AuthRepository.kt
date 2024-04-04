224d839ff171ce63569d387b02d6f3693ff5c449
cuartociclo2023 <cuartocicloidat2023@gmail.com>
2024-04-03 21:56:38 -0500

    repository terminado

--- app/src/main/java/pe/edu/idat/apppatitasidatlc/repository/AuthRepository.kt
+++ app/src/main/java/pe/edu/idat/apppatitasidatlc/repository/AuthRepository.kt
@@ -1,4 +1,45 @@
 package pe.edu.idat.apppatitasidatlc.repository
 
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import pe.edu.idat.apppatitasidatlc.retrofit.PatitasCliente
+import pe.edu.idat.apppatitasidatlc.retrofit.PatitasServicio
+import pe.edu.idat.apppatitasidatlc.retrofit.request.LoginRequest
+import pe.edu.idat.apppatitasidatlc.retrofit.request.RegistroRequest
+import pe.edu.idat.apppatitasidatlc.retrofit.response.LoginResponse
+import pe.edu.idat.apppatitasidatlc.retrofit.response.RegistroResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
 class AuthRepository {
+
+    var loginResponse= MutableLiveData<LoginResponse>()
+    var registroResponse = MutableLiveData<RegistroResponse>()
+
+    fun login(loginRequest: LoginRequest): MutableLiveData<LoginResponse>{
+        val call: Call<LoginResponse> = PatitasCliente.retrofitService.login(loginRequest)
+        call.enqueue(object : Callback<LoginResponse>{
+            override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
+                loginResponse.value = response.body()
+            }
+            override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
+                Log.e("ErrorLogin", t.message.toString())
+            }
+        })
+        return loginResponse
+    }
+
+    fun registro(registroRequest: RegistroRequest): MutableLiveData<RegistroResponse>{
+        val call: Call<RegistroResponse> = PatitasCliente.retrofitService.registrar(registroRequest)
+        call.enqueue(object : Callback<RegistroResponse>{
+            override fun onResponse(call: Call<RegistroResponse>, response: Response<RegistroResponse>) {
+                registroResponse.value = response.body()
+            }
+            override fun onFailure(call: Call<RegistroResponse>, t: Throwable) {
+                Log.e("ErrorLogin", t.message.toString())
+            }
+        })
+        return registroResponse
+    }
 }
--- app/src/main/java/pe/edu/idat/apppatitasidatlc/repository/MascotaRepository.kt
+++ app/src/main/java/pe/edu/idat/apppatitasidatlc/repository/MascotaRepository.kt
@@ -1,4 +1,46 @@
 package pe.edu.idat.apppatitasidatlc.repository
 
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import pe.edu.idat.apppatitasidatlc.retrofit.PatitasCliente
+import pe.edu.idat.apppatitasidatlc.retrofit.request.RegistroRequest
+import pe.edu.idat.apppatitasidatlc.retrofit.request.VoluntarioRequest
+import pe.edu.idat.apppatitasidatlc.retrofit.response.MascotaResponse
+import pe.edu.idat.apppatitasidatlc.retrofit.response.RegistroResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
 class MascotaRepository {
+
+    var mascotaResponse = MutableLiveData<List<MascotaResponse>>()
+    var registroResponse = MutableLiveData<RegistroResponse>()
+
+    fun mascotasPerdidas(): MutableLiveData<List<MascotaResponse>>{
+        val call : Call<List<MascotaResponse>> = PatitasCliente.retrofitService.mascotas()
+        call.enqueue(object : Callback<List<MascotaResponse>>{
+            override fun onResponse(
+                call: Call<List<MascotaResponse>>,
+                response: Response<List<MascotaResponse>>
+            ) {
+               mascotaResponse.value = response.body()
+            }
+            override fun onFailure(call: Call<List<MascotaResponse>>, t: Throwable) {
+                Log.i("ErrorListMascota", t.message.toString())
+            }
+        })
+        return mascotaResponse
+    }
+    fun registroVoluntario(voluntarioRequest: VoluntarioRequest): MutableLiveData<RegistroResponse>{
+        val call: Call<RegistroResponse> = PatitasCliente.retrofitService.registrarVoluntario(voluntarioRequest)
+        call.enqueue(object : Callback<RegistroResponse>{
+            override fun onResponse(call: Call<RegistroResponse>, response: Response<RegistroResponse>) {
+                registroResponse.value = response.body()
+            }
+            override fun onFailure(call: Call<RegistroResponse>, t: Throwable) {
+                Log.e("ErrorLogin", t.message.toString())
+            }
+        })
+        return registroResponse
+    }
 }
