73f6a5c085ac69b2b97b7034e9f140fae45c462a
cuartociclo2023 <cuartocicloidat2023@gmail.com>
2024-04-10 21:09:53 -0500

    Persistencia de datos V2.0

--- app/src/main/java/pe/edu/idat/apppatitasidatlc/view/ui/MainActivity.kt
+++ app/src/main/java/pe/edu/idat/apppatitasidatlc/view/ui/MainActivity.kt
@@ -8,20 +8,25 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import pe.edu.idat.apppatitasidatlc.R
 import pe.edu.idat.apppatitasidatlc.databinding.ActivityMainBinding
+import pe.edu.idat.apppatitasidatlc.model.db.entity.PersonaEntity
 import pe.edu.idat.apppatitasidatlc.retrofit.response.LoginResponse
 import pe.edu.idat.apppatitasidatlc.util.AppMensaje
 import pe.edu.idat.apppatitasidatlc.util.TipoMensaje
 import pe.edu.idat.apppatitasidatlc.viewmodel.AuthViewModel
+import pe.edu.idat.apppatitasidatlc.viewmodel.PersonaViewModel
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityMainBinding
     private lateinit var authViewModel: AuthViewModel
+    private lateinit var personaViewModel: PersonaViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         authViewModel = ViewModelProvider(this)
             .get(AuthViewModel::class.java)
+        personaViewModel = ViewModelProvider(this)
+            .get(PersonaViewModel::class.java)
         binding.btnlogin.setOnClickListener(this)
         binding.btnregistro.setOnClickListener(this)
         authViewModel.loginResponse.observe(this, Observer {
@@ -31,6 +36,10 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private fun obtenerDatosLogin(response: LoginResponse) {
         if(response.rpta){
+            val personaEntity = PersonaEntity(
+                response.idpersona.toInt(), response.nombres, response.apellidos, response.email,
+                response.celular, response.esvoluntario, response.password, response.usuario)
+            personaViewModel.insertar(personaEntity)
             startActivity(Intent(applicationContext, HomeActivity::class.java))
         }else{
             AppMensaje.enviarMensaje(binding.root, response.mensaje, TipoMensaje.ERROR)
