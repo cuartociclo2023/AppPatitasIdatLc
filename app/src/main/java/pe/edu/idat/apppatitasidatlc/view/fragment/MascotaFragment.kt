fbeb5281c35b7aca232496adf225dd9b8e60cbc9
cuartociclo2023 <cuartocicloidat2023@gmail.com>
2024-04-10 19:53:06 -0500

    lista mascotas terminada

--- app/src/main/java/pe/edu/idat/apppatitasidatlc/view/fragment/MascotaFragment.kt
+++ app/src/main/java/pe/edu/idat/apppatitasidatlc/view/fragment/MascotaFragment.kt
@@ -5,18 +5,37 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
+import androidx.recyclerview.widget.LinearLayoutManager
 import pe.edu.idat.apppatitasidatlc.R
+import pe.edu.idat.apppatitasidatlc.databinding.FragmentMascotaBinding
+import pe.edu.idat.apppatitasidatlc.view.adapter.MascotaAdapter
+import pe.edu.idat.apppatitasidatlc.viewmodel.MascotaViewModel
 
 
 class MascotaFragment : Fragment() {
-
+    private var _binding: FragmentMascotaBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var mascotaViewModel: MascotaViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mascota, container, false)
+        _binding = FragmentMascotaBinding.inflate(inflater, container, false)
+        binding.rvmascotas.layoutManager = LinearLayoutManager(requireActivity())
+        mascotaViewModel = ViewModelProvider(requireActivity())
+            .get(MascotaViewModel::class.java)
+        listarMascotas()
+        return binding.root
+    }
+    private fun listarMascotas() {
+        mascotaViewModel.listarMascotas().observe(viewLifecycleOwner,
+            Observer {
+                binding.rvmascotas.adapter = MascotaAdapter(it)
+            })
     }
 
 }
