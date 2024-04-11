868f3fd5e96a8afee570b843b5f506bc7d1bb9c7
cuartociclo2023 <cuartocicloidat2023@gmail.com>
2024-04-10 19:36:24 -0500

    lista mascotas: adapter y viewmodel

--- app/src/main/java/pe/edu/idat/apppatitasidatlc/view/adapter/MascotaAdapter.kt
+++ app/src/main/java/pe/edu/idat/apppatitasidatlc/view/adapter/MascotaAdapter.kt
@@ -1,4 +1,31 @@
 package pe.edu.idat.apppatitasidatlc.view.adapter
 
-class MascotaAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import pe.edu.idat.apppatitasidatlc.databinding.ItemMascotaBinding
+import pe.edu.idat.apppatitasidatlc.retrofit.response.MascotaResponse
+
+class MascotaAdapter(private var listaMascotas: List<MascotaResponse>) : RecyclerView.Adapter<MascotaAdapter.ViewHolder>() {
+    inner class ViewHolder(val binding: ItemMascotaBinding): RecyclerView.ViewHolder(binding.root)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MascotaAdapter.ViewHolder {
+        val binding = ItemMascotaBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
+        return ViewHolder(binding)
+    }
+    override fun onBindViewHolder(holder: MascotaAdapter.ViewHolder, position: Int) {
+        with(holder){
+            with(listaMascotas[position]){
+                binding.tvnommascota.text = nommascota
+                binding.tvfechaperdida.text = fechaperdida
+                binding.tvcontacto.text = contacto
+                binding.tvlugar.text = lugar
+                Glide.with(itemView.context)
+                    .load(urlimagen).into(binding.ivmascota)
+            }
+        }
+    }
+    override fun getItemCount(): Int = listaMascotas.size
 }
--- app/src/main/java/pe/edu/idat/apppatitasidatlc/viewmodel/MascotaViewModel.kt
+++ app/src/main/java/pe/edu/idat/apppatitasidatlc/viewmodel/MascotaViewModel.kt
@@ -1,4 +1,25 @@
 package pe.edu.idat.apppatitasidatlc.viewmodel
 
-class MascotaViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import pe.edu.idat.apppatitasidatlc.repository.MascotaRepository
+import pe.edu.idat.apppatitasidatlc.retrofit.request.VoluntarioRequest
+import pe.edu.idat.apppatitasidatlc.retrofit.response.MascotaResponse
+import pe.edu.idat.apppatitasidatlc.retrofit.response.RegistroResponse
+
+class MascotaViewModel: ViewModel() {
+
+    private var repository = MascotaRepository()
+    var registroResponse: LiveData<RegistroResponse>
+             = repository.registroResponse
+
+    fun listarMascotas(): LiveData<List<MascotaResponse>>{
+        return repository.mascotasPerdidas()
+    }
+
+    fun registrarVoluntario(idPersona: Int){
+        registroResponse = repository.registroVoluntario(VoluntarioRequest(idPersona))
+    }
+
+
 }
