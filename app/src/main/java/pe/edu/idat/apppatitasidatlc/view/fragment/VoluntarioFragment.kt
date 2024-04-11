3dd39fdb28f5454ad0c89894809331e8187d7792
cuartociclo2023 <cuartocicloidat2023@gmail.com>
2024-04-10 20:02:38 -0500

    registro de voluntario v1

--- app/src/main/java/pe/edu/idat/apppatitasidatlc/view/fragment/VoluntarioFragment.kt
+++ app/src/main/java/pe/edu/idat/apppatitasidatlc/view/fragment/VoluntarioFragment.kt
@@ -5,18 +5,34 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
 import pe.edu.idat.apppatitasidatlc.R
+import pe.edu.idat.apppatitasidatlc.databinding.FragmentVoluntarioBinding
+import pe.edu.idat.apppatitasidatlc.viewmodel.MascotaViewModel
 
 
-class VoluntarioFragment : Fragment() {
+class VoluntarioFragment : Fragment(), View.OnClickListener {
 
-
+    private var _binding: FragmentVoluntarioBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var mascotaViewModel: MascotaViewModel
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_voluntario, container, false)
+        _binding = FragmentVoluntarioBinding.inflate(inflater, container, false)
+        mascotaViewModel = ViewModelProvider(requireActivity())
+            .get(MascotaViewModel::class.java)
+        binding.btnregistrarvoluntario.setOnClickListener(this)
+        return binding.root
+    }
+    override fun onClick(v: View?) {
+        if(binding.cbaceptarterminos.isChecked){
+            binding.btnregistrarvoluntario.isEnabled = false
+            //mascotaViewModel.registrarVoluntario()
+        }
     }
 
 }
